b4c5c3d2ded0cd35898f4d5d6a11126fdfa624ad
Ellana Lawrence <elawrence.blain@gmail.com>
2019-11-16 20:07:27 -0500

    9.10 Complete

---
+++ Snacktacular/Double+roundTo.swift
@@ -0,0 +1,20 @@
+//
+//  Double+roundTo.swift
+//  Snacktacular
+//
+//  Created by Ellana Lawrence on 11/16/19.
+//  Copyright © 2019 Ellana Lawrence. All rights reserved.
+//
+
+import Foundation
+
+//rounds any Double to "places" places, e.g. if value = 3.275, value.roundTo (places:1) returns 3.3
+
+extension Double {
+    func roundTo(places: Int) -> Double {
+        let tenToPower = pow(10.0, Double((places >= 0 ? places : 0)))
+        let roundedValue = (self * tenToPower).rounded() / tenToPower
+        return roundedValue
+    }
+    
+}
